cc948c5fc52ad534488f2f680484cbfb8ea84638
micheleferri01 <micheleferri095@gmail.com>
2026-05-01 14:31:18 +0200

    add: function to create string of allowed characters

--- functions.php
+++ functions.php
@@ -1,7 +1,28 @@
 <?php
-    function passwordGenerator ($pass_lenght) {
+    // funzione per generare la stringa dei valori permessi basandosi sulle preferenze indicate  dall'utente
+    function allowed_chars($numbers_allowed,$simbols_allowed,$letters_allowed){
+        $letters = 'ABCDEFGHIJLMNOPQRSTUVWXYZabcdefghijlmnopqrstuvwxyz';
+        $numbers = '0123456789';
+        $simbols = '*@#$&!?/';
+        $allowedChars = '';
+
+        if($letters_allowed == 'on'){
+            $allowedChars = $allowedChars . $letters;
+        }
+        if($numbers_allowed == 'on'){
+             $allowedChars = $allowedChars . $numbers;
+        }
+        if($simbols_allowed == 'on'){
+            $allowedChars = $allowedChars . $simbols;
+        }
+
+        return $allowedChars;
+    }
+
+    
+    function passwordGenerator ($pass_lenght,$doubled_chars_allowed) {
         // Stringa dei valori ammessi
-        $allowedChars = 'ABCDEFGHIJKYLMNOPQRSTUVWZabcdefghilmnopqrstuvzwyxkj0123456789*/$!?^#@';
+        $allowedChars = '';
         
         // Array per salvare la password che verrà creata
         $generated_password = [];
