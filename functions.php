6b44d22e2583ff6898e54f7e5a4578bf73eb22a1
micheleferri01 <micheleferri095@gmail.com>
2026-05-01 17:02:45 +0200

    add: logic for the new params

--- functions.php
+++ functions.php
@@ -6,32 +6,47 @@
         $simbols = '*@#$&!?/';
         $allowedChars = '';
 
-        if($letters_allowed == 'on'){
+        if($letters_allowed == 'true'){
             $allowedChars = $allowedChars . $letters;
         }
-        if($numbers_allowed == 'on'){
+        if($numbers_allowed == 'true'){
              $allowedChars = $allowedChars . $numbers;
         }
-        if($simbols_allowed == 'on'){
+        if($simbols_allowed == 'true'){
             $allowedChars = $allowedChars . $simbols;
         }
 
         return $allowedChars;
     }
 
-    
-    function passwordGenerator ($pass_lenght,$doubled_chars_allowed) {
-        // Stringa dei valori ammessi
-        $allowedChars = '';
+
+    function passwordGenerator ($pass_lenght,$doubled_chars_allowed, $allowedChars) {
         
         // Array per salvare la password che verrà creata
         $generated_password = [];
         
-        if($pass_lenght && $pass_lenght !== null){
-            while (count($generated_password) < $pass_lenght){
-                $n = rand(0, strlen($allowedChars)-1 );
-                array_push($generated_password, $allowedChars[$n]);
+        if(!empty($pass_lenght) && $pass_lenght > 0){
+            if($doubled_chars_allowed == 'Si'){
+                while (count($generated_password) < $pass_lenght){
+                    $n = rand(0, strlen($allowedChars)-1 );
+                    array_push($generated_password, $allowedChars[$n]);
+                }
+            }else{
+                 if($pass_lenght <= strlen($allowedChars)-1){
+                    while (count($generated_password) < $pass_lenght){
+                        $n = rand(0, strlen($allowedChars)-1 );
+                        if(!in_array($allowedChars[$n], $generated_password)){
+                            array_push($generated_password, $allowedChars[$n]);
+                        }
+                    }
+                }else{
+                    $length_allowedChars = strlen($allowedChars)-1;
+                    $error = "Lunghezza troppo grande senza duplicati.";
+
+                    return $error;
+                }
             }
+           
 
             return implode($generated_password);
         }
