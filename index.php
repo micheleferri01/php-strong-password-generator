b33385a2d2f38693027ec915dc8ecfa2987c45af
micheleferri01 <micheleferri095@gmail.com>
2026-05-01 12:29:13 +0200

    add: function to generate a password given the length of it

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<?php 
+$password_lenght = isset($_GET['password_lenght']) && $_GET['password_lenght']!== ''? (int)$_GET['password_lenght'] : null;
+
+function passwordGenerator ($pass_lenght) {
+    // Stringa dei valori ammessi
+    $allowedChars = 'ABCDEFGHIJKYLMNOPQRSTUVWZabcdefghilmnopqrstuvzwyxkj0123456789*/$!?^#@';
+    
+    // Array per salvare la password che verrà creata
+    $generated_password = [];
+    
+    if($pass_lenght && $pass_lenght !== null){
+        while (count($generated_password) < $pass_lenght){
+            $n = rand(0, strlen($allowedChars)-1 );
+            array_push($generated_password, $allowedChars[$n]);
+        }
+
+        return implode($generated_password);
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php strong password generator</title>
+</head>
+<body>
+    <h1>Strong password generator</h1>
+    <h2>Genera la tua password sicura</h2>
+
+    <div>
+        <form action="" mehtod='GET'>
+            <div>
+                <label for="pass-lenght">Lunghezza password</label>
+                <input type="number" id='pass-lenght' name='password_lenght' required>
+            </div>
+
+            <button type='submit'>Genera password</button>
+        </form>
+
+        <p>Password generata: <?php echo passwordGenerator($password_lenght); ?></p>
+    </div>
+</body>
+</html>
