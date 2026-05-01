72f072b204575ee94532ef41044080a364dae9eb
micheleferri01 <micheleferri095@gmail.com>
2026-05-01 17:04:36 +0200

    add: logic to handle possible errors in the compiled form data send

--- result.php
+++ result.php
@@ -1,7 +1,19 @@
 <?php
+require_once('./functions.php');
+
+$error = null;
+
 $password_lenght = isset($_GET['password_lenght']) && $_GET['password_lenght']!== ''? (int)$_GET['password_lenght'] : null;
 
-require_once('./functions.php');
+$doubled_chars_allowed = $_GET['doubled_chars_allowed']?? null;
+
+$letters_allowed = !empty($_GET['letters']);
+$numbers_allowed = !empty($_GET['numbers']);
+$simbols_allowed = !empty($_GET['simbols']);
+
+// stringa dei valori permessi
+$allowedChars = null;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +24,18 @@ require_once('./functions.php');
 </head>
 <body>
     <h1>Password generata</h1>
-    <p>Password generata: <?php echo passwordGenerator($password_lenght); ?></p>
+    <?php
+        if (!$letters_allowed && !$numbers_allowed && !$simbols_allowed) {
+            $error = 'Devi selezionare almeno un tipo di carattere';
+           echo "<p>$error</p>";
+        } elseif (empty($password_lenght) || $password_lenght <= 0) {
+            $error = 'Lunghezza password non valida';
+            echo "<p>$error</p>";
+        } else {
+            $allowedChars = allowed_chars($numbers_allowed, $simbols_allowed, $letters_allowed);
+            $generatedPassword = passwordGenerator($password_lenght, $doubled_chars_allowed, $allowedChars);
+            echo "<p>$generatedPassword</p>";
+        }
+    ?>
 </body>
 </html>
